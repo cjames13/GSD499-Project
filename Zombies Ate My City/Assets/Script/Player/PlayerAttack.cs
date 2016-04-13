c03876bc62fbeb4b47ec3f25735d758a715a33a2
rskill01 <robert.skillen@cox.net>
2016-04-13 12:08:01 -0400

    Finished gun shooting
    check if you like it

--- Zombies Ate My City/Assets/Script/Player/PlayerAttack.cs
+++ Zombies Ate My City/Assets/Script/Player/PlayerAttack.cs
@@ -5,11 +5,6 @@ public class PlayerAttack : MonoBehaviour {
 	public GameObject projectileObject;
 	public Transform firePoint;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown ("Fire1")) {
