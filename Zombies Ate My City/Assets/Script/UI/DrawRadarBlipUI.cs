43a9b1dd8fb01f1bf6e8664c5d91ec8128f204fd
cjames13 <cjames13@baker.edu>
2016-04-14 15:28:39 -0400

    Radar finished.

--- Zombies Ate My City/Assets/Script/UI/DrawRadarBlipUI.cs
+++ Zombies Ate My City/Assets/Script/UI/DrawRadarBlipUI.cs
@@ -9,10 +9,30 @@ public class DrawRadarBlipUI : MonoBehaviour {
 	public Transform centerAround;
 	public float mapScale = 0.3f;
 
+
 	private Vector2 mapCenter;
+	private Texture2D blipTexture;
 
 	void Start() {
-		mapCenter = GetComponent<RectTransform> ().sizeDelta;
+		//mapCenter = GetComponent<RectTransform> ().transform.position;
+		RectTransform rect = GetComponent<RectTransform>();
+		mapCenter = rect.sizeDelta / 2;
+		mapCenter.x += rect.offsetMin.x / 2;
+		mapCenter.y += rect.offsetMax.y;
+
+		CreateBlipTexture ();
+	}
+
+	void CreateBlipTexture() {
+		blipTexture = new Texture2D (blipSize, blipSize);
+		Color[] c = blipTexture.GetPixels ();
+
+		for (int i = 0; i < c.Length; i++) {
+			c[i] = blipColor;
+		}
+
+		blipTexture.SetPixels (c);
+		blipTexture.Apply ();
 	}
 
 	void OnGUI() {
@@ -40,7 +60,9 @@ public class DrawRadarBlipUI : MonoBehaviour {
 			bX *= mapScale;
 			bY *= mapScale;
 
-			GUI.Box(new Rect(mapCenter.x + bX, mapCenter.y + bY, blipSize, blipSize), "a");
+			//GUI.Box(new Rect(mapCenter.x + bX, mapCenter.y + bY, blipSize, blipSize), blipTexture);
+			GUI.DrawTexture(new Rect(mapCenter.x + bX, mapCenter.y + bY, blipSize, blipSize), blipTexture);
+	
 		}
 	}
 }
