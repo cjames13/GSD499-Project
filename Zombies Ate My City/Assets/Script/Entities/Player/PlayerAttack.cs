11a03f9399ba52830c4c33d8757632a2c9fbb9d4
cjames13 <cjames13@baker.edu>
2016-04-13 22:09:30 -0400

    PlayerAttack Lerp fix.

--- Zombies Ate My City/Assets/Script/Entities/Player/PlayerAttack.cs
+++ Zombies Ate My City/Assets/Script/Entities/Player/PlayerAttack.cs
@@ -4,14 +4,46 @@ using System.Collections;
 public class PlayerAttack : MonoBehaviour {
 	public GameObject projectileObject;
 	public Transform firePoint;
+	public float firingSpeed = 0.125f;
+	public float drawTime = 20f;
+	public float holsterTime = 10f;
 
-	// Update is called once per frame
+	Animator anim;
+	float currentWeight = 0;
+	float time = 0;
+
+	float animationTime;
+	bool draw = false;
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
 	void Update () {
-		if (Input.GetButtonDown ("Fire1")) {
-			// Shoot from end of gun
-			Instantiate (projectileObject, firePoint.position, firePoint.transform.rotation);
-			// Always shoot forward (for testing)
- 			//Instantiate (projectileObject, firePoint.position, Quaternion.identity);
+		if (Input.GetButton ("Fire1")) {
+			time -= Time.deltaTime;
+
+			if (time <= 0) {
+				Instantiate (projectileObject, firePoint.position, firePoint.transform.rotation);
+				time = firingSpeed;
+			}
+
+			if (!draw) {
+				draw = true;
+				animationTime = 0f;
+			}
+		} else {
+			draw = false;
 		}
+
+		animationTime += Time.deltaTime;
+		float thisTime = (draw) ? drawTime : holsterTime;
+
+		if (animationTime > thisTime) {
+			animationTime = thisTime;
+		}
+
+		currentWeight = Mathf.Lerp (currentWeight, (draw) ? 1.0f : 0.0f, animationTime / thisTime);
+		anim.SetLayerWeight(2, currentWeight);
 	}
 }
