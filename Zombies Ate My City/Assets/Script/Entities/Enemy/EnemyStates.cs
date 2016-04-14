7a4ece2bf8617c9f2da1fff0f50d7c5ec4b7d866
cjames13 <cjames13@baker.edu>
2016-04-14 16:07:03 -0400

    Various bugfixes.

--- Zombies Ate My City/Assets/Script/Entities/Enemy/EnemyStates.cs
+++ Zombies Ate My City/Assets/Script/Entities/Enemy/EnemyStates.cs
@@ -2,62 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 public class EnemyStates : MonoBehaviour, StateController {
-	int health = 1;
-	Rigidbody rigidBody;
-	Rigidbody[] rigidBodies;
-	CapsuleCollider myCollider;
-	BoxCollider myBoxCollider;
-	SphereCollider mySphereCollider;
-	List<Collider> colliders;
-	Animator anim;
-	StateController stateController;
-	void Awake(){
-		if (GetComponent<BoxCollider> ())
-			myBoxCollider = GetComponent<BoxCollider> ();
-		if (GetComponent<SphereCollider> ())
-			mySphereCollider = GetComponent<SphereCollider> ();
-		anim = GetComponent<Animator> ();
-		rigidBody = GetComponent<Rigidbody>();
-		rigidBodies = GetComponentsInChildren<Rigidbody> ();
-		myCollider = GetComponent<CapsuleCollider> ();
-		foreach (Rigidbody rb in rigidBodies) {
-			if (rb != rigidBody)
-				rb.isKinematic = true;
-		}
-		rigidBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
-		SetAllChildCollidersTrigger (true);
-	}
 
-	void SetAllChildCollidersTrigger(bool t) {
-		foreach (Collider c in GetComponentsInChildren<Collider>()) {
-			if (c != myCollider && c != myBoxCollider && c != mySphereCollider)
-				c.isTrigger = t;
-		}
-	}
-	void OnCollisionEnter(Collision other)
-	{
-		if (other.transform.tag == "PlayerAttack") {
-			stateController.TakeDamage ();
-		}
-	}
 	void StateController.TakeDamage() {
-		health -= 1;
-		if (health == 0) {
-			stateController.Die ();
-			SetAllChildCollidersTrigger (false);
-		}
 		// Damage taken animation here
 	}
 
 	void StateController.Die() {
-		myCollider.enabled = false;
+		/*myCollider.enabled = false;
 		rigidBody.useGravity = false;
 		foreach (Rigidbody rb in rigidBodies) {
 			rb.isKinematic = false;
 		}
 		anim.enabled = false;
-		SetAllChildCollidersTrigger (false);
-		//Destroy (gameObject);
+		SetAllChildCollidersTrigger (false);*/
+		Destroy (gameObject);
 	}
 
 }
---
+++ Zombies Ate My City/Assets/Script/Items/Resources.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resources : MonoBehaviour {
+	private bool isCollected = false;
+	private GameController gameController;
+
+	void Start() {
+		gameController = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController> ();;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player" && !isCollected) {
+			isCollected = true;
+			gameController.ResourceCollected ();
+		}
+	}
+}
