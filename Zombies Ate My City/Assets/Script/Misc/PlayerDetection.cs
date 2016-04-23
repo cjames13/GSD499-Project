8b1a01bfa6ce7beaf8f9473db352a86187038e3b
Tyler <tburns517@yahoo.com>
2016-04-22 23:09:20 -0400

    Level Enhancement
    - More Spawn Points with Different Enemies
    - Spawn Points have Player Detection Script
    - Particle Effects for Better Atmosphere

---
+++ Zombies Ate My City/Assets/Script/Misc/PlayerDetection.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDetection : MonoBehaviour {
+
+    Spawner spawnerScript;
+
+    void Start()
+    {
+        spawnerScript = GetComponent<Spawner>();
+        spawnerScript.enabled = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            spawnerScript.enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            spawnerScript.enabled = false;
+        }
+    }
+}
