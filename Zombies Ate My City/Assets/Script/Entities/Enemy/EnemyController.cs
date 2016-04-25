74a2916fdb27c79244e545c53d6844d91a279ba5
rskill01 <rskill01@baker.edu>
2016-04-24 21:24:03 -0400

    Dying animation update

--- Zombies Ate My City/Assets/Script/Entities/Enemy/EnemyController.cs
+++ Zombies Ate My City/Assets/Script/Entities/Enemy/EnemyController.cs
@@ -8,14 +8,14 @@ public class EnemyController : MonoBehaviour {
 	public float rotationSpeed = 3f;
 	public float attackSpeed = 1f;
 	public int scoreValue;
-
+	private Animator anim;
 	private NavMeshAgent agent;
 	private StateController enemyStates;
 
 	void Start () {
 		enemyStates = GetComponent<StateController> ();
 		agent = GetComponent<NavMeshAgent> ();
-
+		anim = GetComponent<Animator> ();
 		if (target == null) {
 			target = GameObject.FindGameObjectWithTag ("Player");
 		}
@@ -23,33 +23,35 @@ public class EnemyController : MonoBehaviour {
 	
 
 	void Update () {
-		float attackDistance = Vector3.Distance (agent.nextPosition, target.transform.position);
-		agent.SetDestination (target.transform.position);
-		Vector3 lookPos = target.transform.position - transform.position;
-		lookPos.y = 0;
-		if (lookPos != Vector3.zero) {
-			Quaternion rotation = Quaternion.LookRotation (lookPos);
-			transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * rotationSpeed);
-		}
-		enemyStates.Walk ();
+		if (anim.GetBool ("dying") != true) {
+			float attackDistance = Vector3.Distance (agent.nextPosition, target.transform.position);
+			agent.SetDestination (target.transform.position);
+			Vector3 lookPos = target.transform.position - transform.position;
+			lookPos.y = 0;
+			if (lookPos != Vector3.zero) {
+				Quaternion rotation = Quaternion.LookRotation (lookPos);
+				transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+			}
+			enemyStates.Walk ();
 
-		if (attackDistance < agent.stoppingDistance) {
-			enemyStates.MeleeAttack (true);
-		} else {
-			enemyStates.MeleeAttack (false);
-		}
-
-		if (rangedAttacker) {
-			if (attackDistance <= agent.stoppingDistance)
+			if (attackDistance < agent.stoppingDistance) {
 				enemyStates.MeleeAttack (true);
-			else
+			} else {
 				enemyStates.MeleeAttack (false);
-			if (attackDistance <= agent.stoppingDistance + magicAttackDistance && 
-				attackDistance >= agent.stoppingDistance)
-				enemyStates.RangedAttack (true);
-			else
-				enemyStates.RangedAttack (false);
-		}
+			}
+
+			if (rangedAttacker) {
+				if (attackDistance <= agent.stoppingDistance)
+					enemyStates.MeleeAttack (true);
+				else
+					enemyStates.MeleeAttack (false);
+				if (attackDistance <= agent.stoppingDistance + magicAttackDistance &&
+				   attackDistance >= agent.stoppingDistance)
+					enemyStates.RangedAttack (true);
+				else
+					enemyStates.RangedAttack (false);
+			}
 		
+		}
 	}
 }
