9a3c75a67ef4d390dede8d25a14d05782fb35e2d
cjames13 <cjames13@baker.edu>
2016-04-29 13:11:13 -0400

    Merge conflict fix.

--- Zombies Ate My City/Assets/Script/Entities/Enemy/EnemyController.cs
+++ Zombies Ate My City/Assets/Script/Entities/Enemy/EnemyController.cs
@@ -4,9 +4,7 @@ using System.Collections;
 public class EnemyController : MonoBehaviour {
 	public GameObject target;
 	public bool rangedAttacker = false;
-	public GameObject projectileObject;
 	public float magicAttackDistance = 8f;
-	public float magicAttackDelay = 2f;
 	public float rotationSpeed = 3f;
 	public float attackSpeed = 1f;
 	public int scoreValue;
@@ -15,18 +13,12 @@ public class EnemyController : MonoBehaviour {
 	private NavMeshAgent agent;
 	private StateController enemyStates;
 	private Health health;
-	private float magicAttackTime;
 
 	void Start () {
 		enemyStates = GetComponent<StateController> ();
 		health = GetComponent<Health> ();
 		agent = GetComponent<NavMeshAgent> ();
 		anim = GetComponent<Animator> ();
-
-		if (rangedAttacker) {
-			magicAttackTime = Time.time;
-		}
-
 		if (target == null) {
 			target = GameObject.FindGameObjectWithTag ("Player");
 		}
@@ -51,14 +43,18 @@ public class EnemyController : MonoBehaviour {
 				enemyStates.MeleeAttack (false);
 			}
 
-			if (rangedAttacker && attackDistance <= agent.stoppingDistance + magicAttackDistance &&
-				attackDistance >= agent.stoppingDistance && Time.time - magicAttackTime > magicAttackDelay) {
-				magicAttackTime = Time.time;
-				enemyStates.RangedAttack (true, false);
-				Instantiate (projectileObject,
-					new Vector3 (transform.position.x, transform.position.y + 0.5f,
-						transform.position.z - 1), transform.rotation);
+			if (rangedAttacker) {
+				if (attackDistance <= agent.stoppingDistance)
+					enemyStates.MeleeAttack (true);
+				else
+					enemyStates.MeleeAttack (false);
+				if (attackDistance <= agent.stoppingDistance + magicAttackDistance &&
+				    attackDistance >= agent.stoppingDistance)
+					enemyStates.RangedAttack (true, false);
+				else
+					enemyStates.RangedAttack (false, false);
 			}
+		
 		} else {
 			agent.velocity = Vector3.zero;
 			gameObject.GetComponent<NavMeshAgent> ().enabled = false;
--- Zombies Ate My City/Assets/Script/Entities/Enemy/EnemyStates.cs
+++ Zombies Ate My City/Assets/Script/Entities/Enemy/EnemyStates.cs
@@ -10,6 +10,12 @@ public class EnemyStates : MonoBehaviour, StateController {
 	private EnemyController enemyController;
 	private GameController gameController;
 
+	private bool playing = true;
+	private bool shooting = true;
+
+	public float magicAttackDelay = 7f;
+	private float magicAttackTime;
+
 	//Ragdoll
 	private Rigidbody rigidBody;
 	private Rigidbody[] rigidBodies;
@@ -20,13 +26,8 @@ public class EnemyStates : MonoBehaviour, StateController {
 	void Start(){
 		gameController = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController> ();
 		anim = GetComponent<Animator> ();
+		magicAttackTime = magicAttackDelay;
 		enemyController = GetComponent<EnemyController> ();
-		anim.SetLayerWeight (1, 1f);
-		anim.SetLayerWeight (2, 1f);
-		if (enemyController.rangedAttacker) {
-			anim.SetLayerWeight (3, 1f);
-		}
-
 		sphereCollider = GetComponent<SphereCollider> ();
 		rigidBody = GetComponent<Rigidbody> ();
 		rigidBodies = GetComponentsInChildren<Rigidbody> ();
@@ -51,7 +52,7 @@ public class EnemyStates : MonoBehaviour, StateController {
 	}
 	void StateController.TakeDamage() {
 		// Damage taken animation here
-		anim.SetTrigger("hurt");
+		StartCoroutine (SetDamageLayerWeight ());
 	}
 
 	void StateController.Die() {
@@ -75,13 +76,62 @@ public class EnemyStates : MonoBehaviour, StateController {
 		gameController.IncreaseScore (enemyController.scoreValue);
 		StartCoroutine (Burning ());
 	}
+	IEnumerator SetDamageLayerWeight() {
+		if (anim)
+		anim.SetLayerWeight (1, 1);
+		yield return new WaitForSeconds(1);
+		if (anim)
+		anim.SetLayerWeight (1, 0);
+	}
 
 	void StateController.MeleeAttack(bool attacking){
-		anim.SetBool ("attacking", attacking);
+		if (anim) {
+			if (anim.GetLayerWeight (1) == 1)
+				attacking = false;
+			if (attacking) {
+				anim.SetLayerWeight (2, 1);
+				if (anim.layerCount > 3)
+					anim.SetLayerWeight (3, 0);
+			} else {
+				anim.SetLayerWeight (2, 0);
+			}
+		}
 	}
 
 	void StateController.RangedAttack(bool attacking, bool isRifle){
-		anim.SetTrigger ("cast");
+		if (anim) {
+			if (anim.GetLayerWeight (1) == 1)
+				attacking = false;
+			if (attacking) {
+				magicAttackTime -= Time.deltaTime;
+				if (magicAttackTime <= 2) {
+					AnimatorStateInfo currentState = anim.GetCurrentAnimatorStateInfo (3);
+					float playbackTime = currentState.normalizedTime % 1;
+					if (playbackTime > 0.9f)
+						playing = false;
+					if (playing == true) {
+						if (shooting == true) {
+							Instantiate (rangedAttackObject,
+								new Vector3 (transform.position.x, transform.position.y + 0.5f,
+									transform.position.z - 1), transform.rotation);
+							shooting = false;
+						}
+						anim.SetLayerWeight (3, 1);
+					} else {
+						anim.SetLayerWeight (3, 0);
+					}
+					if (magicAttackTime <= 0) {
+						playing = true;
+						shooting = true;
+						magicAttackTime = magicAttackDelay;
+					}
+				} else {
+					anim.SetLayerWeight (3, 0);
+				}
+			} else {
+				anim.SetLayerWeight (3, 0);
+			}
+		}
 	}
 
 	void StateController.ThrownAttack(bool attacking) {}
