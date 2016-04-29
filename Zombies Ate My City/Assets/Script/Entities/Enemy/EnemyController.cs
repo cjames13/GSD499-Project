741967782bbc704a63ace42cd4a496896c3c635d
cjames13 <cjames13@baker.edu>
2016-04-29 13:37:00 -0400

    Rolleback.

--- Zombies Ate My City/Assets/Script/Entities/Enemy/EnemyController.cs
+++ Zombies Ate My City/Assets/Script/Entities/Enemy/EnemyController.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class EnemyController : MonoBehaviour {
 	public GameObject target;
 	public bool rangedAttacker = false;
+	public GameObject projectileObject;
 	public float magicAttackDistance = 8f;
+	public float magicAttackDelay = 2f;
 	public float rotationSpeed = 3f;
 	public float attackSpeed = 1f;
 	public int scoreValue;
@@ -13,12 +15,18 @@ public class EnemyController : MonoBehaviour {
 	private NavMeshAgent agent;
 	private StateController enemyStates;
 	private Health health;
+	private float magicAttackTime;
 
 	void Start () {
 		enemyStates = GetComponent<StateController> ();
 		health = GetComponent<Health> ();
 		agent = GetComponent<NavMeshAgent> ();
 		anim = GetComponent<Animator> ();
+
+		if (rangedAttacker) {
+			magicAttackTime = Time.time;
+		}
+
 		if (target == null) {
 			target = GameObject.FindGameObjectWithTag ("Player");
 		}
@@ -43,18 +51,14 @@ public class EnemyController : MonoBehaviour {
 				enemyStates.MeleeAttack (false);
 			}
 
-			if (rangedAttacker) {
-				if (attackDistance <= agent.stoppingDistance)
-					enemyStates.MeleeAttack (true);
-				else
-					enemyStates.MeleeAttack (false);
-				if (attackDistance <= agent.stoppingDistance + magicAttackDistance &&
-				    attackDistance >= agent.stoppingDistance)
-					enemyStates.RangedAttack (true, false);
-				else
-					enemyStates.RangedAttack (false, false);
+			if (rangedAttacker && attackDistance <= agent.stoppingDistance + magicAttackDistance &&
+				attackDistance >= agent.stoppingDistance && Time.time - magicAttackTime > magicAttackDelay) {
+				magicAttackTime = Time.time;
+				enemyStates.RangedAttack (true, false);
+				Instantiate (projectileObject,
+					new Vector3 (transform.position.x, transform.position.y + 0.5f,
+						transform.position.z - 1), transform.rotation);
 			}
-		
 		} else {
 			agent.velocity = Vector3.zero;
 			gameObject.GetComponent<NavMeshAgent> ().enabled = false;
