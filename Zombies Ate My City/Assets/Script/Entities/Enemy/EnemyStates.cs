79409d90c105eb17969a9749e5d12e3380129e8f
cjames13 <cjames13@baker.edu>
2016-04-28 22:55:59 -0400

    Huge EnemyState/EnemyController clean up. Complete enemy animation overhaul.

--- Zombies Ate My City/Assets/Script/Entities/Enemy/EnemyStates.cs
+++ Zombies Ate My City/Assets/Script/Entities/Enemy/EnemyStates.cs
@@ -10,12 +10,6 @@ public class EnemyStates : MonoBehaviour, StateController {
 	private EnemyController enemyController;
 	private GameController gameController;
 
-	private bool playing = true;
-	private bool shooting = true;
-
-	public float magicAttackDelay = 7f;
-	private float magicAttackTime;
-
 	//Ragdoll
 	private Rigidbody rigidBody;
 	private Rigidbody[] rigidBodies;
@@ -26,8 +20,13 @@ public class EnemyStates : MonoBehaviour, StateController {
 	void Start(){
 		gameController = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController> ();
 		anim = GetComponent<Animator> ();
-		magicAttackTime = magicAttackDelay;
 		enemyController = GetComponent<EnemyController> ();
+		anim.SetLayerWeight (1, 1f);
+		anim.SetLayerWeight (2, 1f);
+		if (enemyController.rangedAttacker) {
+			anim.SetLayerWeight (3, 1f);
+		}
+
 		sphereCollider = GetComponent<SphereCollider> ();
 		rigidBody = GetComponent<Rigidbody> ();
 		rigidBodies = GetComponentsInChildren<Rigidbody> ();
@@ -52,7 +51,7 @@ public class EnemyStates : MonoBehaviour, StateController {
 	}
 	void StateController.TakeDamage() {
 		// Damage taken animation here
-		StartCoroutine (SetDamageLayerWeight ());
+		anim.SetTrigger("hurt");
 	}
 
 	void StateController.Die() {
@@ -76,62 +75,13 @@ public class EnemyStates : MonoBehaviour, StateController {
 		gameController.IncreaseScore (enemyController.scoreValue);
 		StartCoroutine (Burning ());
 	}
-	IEnumerator SetDamageLayerWeight() {
-		if (anim)
-		anim.SetLayerWeight (1, 1);
-		yield return new WaitForSeconds(1);
-		if (anim)
-		anim.SetLayerWeight (1, 0);
-	}
 
 	void StateController.MeleeAttack(bool attacking){
-		if (anim) {
-			if (anim.GetLayerWeight (1) == 1)
-				attacking = false;
-			if (attacking) {
-				anim.SetLayerWeight (2, 1);
-				if (anim.layerCount > 3)
-					anim.SetLayerWeight (3, 0);
-			} else {
-				anim.SetLayerWeight (2, 0);
-			}
-		}
+		anim.SetBool ("attacking", attacking);
 	}
 
 	void StateController.RangedAttack(bool attacking, bool isRifle){
-		if (anim) {
-			if (anim.GetLayerWeight (1) == 1)
-				attacking = false;
-			if (attacking) {
-				magicAttackTime -= Time.deltaTime;
-				if (magicAttackTime <= 2) {
-					AnimatorStateInfo currentState = anim.GetCurrentAnimatorStateInfo (3);
-					float playbackTime = currentState.normalizedTime % 1;
-					if (playbackTime > 0.9f)
-						playing = false;
-					if (playing == true) {
-						if (shooting == true) {
-							Instantiate (rangedAttackObject,
-								new Vector3 (transform.position.x, transform.position.y + 0.5f,
-									transform.position.z - 1), transform.rotation);
-							shooting = false;
-						}
-						anim.SetLayerWeight (3, 1);
-					} else {
-						anim.SetLayerWeight (3, 0);
-					}
-					if (magicAttackTime <= 0) {
-						playing = true;
-						shooting = true;
-						magicAttackTime = magicAttackDelay;
-					}
-				} else {
-					anim.SetLayerWeight (3, 0);
-				}
-			} else {
-				anim.SetLayerWeight (3, 0);
-			}
-		}
+		anim.SetTrigger ("cast");
 	}
 
 	void StateController.ThrownAttack(bool attacking) {}
