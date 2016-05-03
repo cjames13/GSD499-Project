a031e223a418470324792e06d486118482457e14
Tyler <tburns517@yahoo.com>
2016-05-03 00:54:23 -0400

    Intro Video Scene Complete

---
+++ Zombies Ate My City/Assets/Script/Misc/Video.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class Video : MonoBehaviour
+{
+    public Text videoSkipText;
+    public MovieTexture movie;
+    private new AudioSource audio;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Renderer>().material.mainTexture = movie as MovieTexture;
+        audio = GetComponent<AudioSource>();
+
+        movie.Play();
+        audio.Play();       
+    }
+
+    void Update()
+    {
+        StartCoroutine(VideoSkip());
+        if (!movie.isPlaying || Input.GetKeyDown(KeyCode.S))
+        {
+            SceneManager.LoadScene("Start Screen");
+        }
+    }
+
+    IEnumerator VideoSkip()
+    {
+        yield return new WaitForSeconds(5);
+        videoSkipText.text = "Press 'S' to Skip";
+    }
+
+
+
+}
