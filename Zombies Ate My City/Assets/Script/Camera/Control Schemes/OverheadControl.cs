721a55b35d111b4cda5a1470eebe6db929731f97
rskill01 <rskill01@baker.edu>
2016-05-04 18:19:38 -0400

    pushing

--- Zombies Ate My City/Assets/Script/Camera/Control Schemes/OverheadControl.cs
+++ Zombies Ate My City/Assets/Script/Camera/Control Schemes/OverheadControl.cs
@@ -4,8 +4,7 @@ using System.Collections;
 public class OverheadControl : ControlStrategy {
 	// This sets the player's movement. Whatever value is returned from this function will be added to the player's current position
 	public Vector3 SetPlayerMovement(float horizontalInput, float verticalInput, Vector3 moveDirection, float moveSpeed, float movementPenalty) {
-		float finalMoveSpeed = moveSpeed * ((Mathf.Abs (horizontalInput) > 0 || verticalInput < 0) ? movementPenalty : 1);
-		return Vector3.ClampMagnitude (moveDirection * Time.deltaTime * finalMoveSpeed, finalMoveSpeed);
+		return Vector3.ClampMagnitude (moveDirection,moveSpeed) * Time.deltaTime * moveSpeed;
 	}
 
 	public Quaternion SetPlayerRotation(Camera camera, Vector3 moveDirection, bool isRolling) {
@@ -14,7 +13,7 @@ public class OverheadControl : ControlStrategy {
 
 	// The animation for the player's movement goes here
 	public void SetPlayerMovementAnimation(Animator animator, float horizontalInput, float verticalInput, bool isShooting) {
-		animator.SetFloat ("HorizontalVelocity", horizontalInput);
+		animator.SetFloat ("VerticalVelocity", Mathf.Abs(horizontalInput));
 		animator.SetFloat ("VerticalVelocity", verticalInput);
 	}
 }
