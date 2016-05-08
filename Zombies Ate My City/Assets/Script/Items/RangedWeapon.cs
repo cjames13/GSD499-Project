81bc5314cba0fba68c102c2c25586e95cfe1b116
rskill01 <rskill01@baker.edu>
2016-05-08 17:04:59 -0400

    rifles minor rotation change
    Did the best I could

--- Zombies Ate My City/Assets/Script/Items/RangedWeapon.cs
+++ Zombies Ate My City/Assets/Script/Items/RangedWeapon.cs
@@ -12,16 +12,17 @@ public class RangedWeapon : Weapon {
 	private AudioSource muzzleSound;
 	private Animator muzzleLight;
 	void Start() {
+		attackLocation = GameObject.FindGameObjectWithTag ("AttackLocation").transform;
 		if (muzzle != null) {
 			muzzleFlash = muzzle.GetComponent<ParticleSystem> ();
 			muzzleSound = muzzle.GetComponent<AudioSource> ();
 			muzzleLight = muzzle.GetComponent<Animator> ();
 		}
-		attackLocation = GameObject.FindGameObjectWithTag ("AttackLocation").transform;
 		currentAmmo = maxAmmo;
 	}
 
 	public override void Attack() {
+		
 		if (attackTime <= 0 && currentAmmo > 0) {
 			Instantiate (projectileObject, attackLocation.position, attackLocation.rotation);
 			MuzzleFlash ();
@@ -29,7 +30,6 @@ public class RangedWeapon : Weapon {
 			attackTime = attackSpeed;
 		}
 	}
-
 	public override void PlayAnimation(StateController stateController, bool attacking) {
 		stateController.RangedAttack (attacking, isRifle);
 	}
